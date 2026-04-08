b359c5bddfb825c70ace47ddab7f69cb8b489197
Saad-Driouech <saaddriouech1@gmail.com>
2026-04-08 12:21:21 +0200

    Add gray modality ID to control_data.py MODALITY_IDS

--- utils/control_data.py
+++ utils/control_data.py
@@ -23,7 +23,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.transforms import InterpolationMode
 
-MODALITY_IDS = {"canny": 0, "depth": 1, "seg": 2, "normal": 3, "hed": 4}
+MODALITY_IDS = {"canny": 0, "depth": 1, "seg": 2, "normal": 3, "hed": 4, "gray": 5}
 
 
 def _build_transform(final_reso: int, mid_reso_factor: float = 1.125):
